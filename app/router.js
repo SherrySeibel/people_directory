dbf4bef9de9cdf6dc3e9403f762edbf78671adcc
Sherry Seibel <sherry.seibel@gmail.com>
2015-02-05 21:49:01 -0500

    ember new people_directory

--- app/router.js
+++ app/router.js
@@ -1,11 +1,14 @@
-import Ember from 'ember';
-import config from './config/environment';
+import Ember from "ember";
+import config from "./config/environment";
 
 var Router = Ember.Router.extend({
   location: config.locationType
 });
 
 Router.map(function() {
+  this.resource("people", function() {
+    this.resource("people", { path: "/:people_id" });
+  });
 });
 
 export default Router;
